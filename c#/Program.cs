288cc40c18e9c6c180a27de4a8315360c933f737
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-11 15:55:10 +0300

    Add files via upload

---
+++ c#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace deneme {
+    class Program{
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("İsminizi Girin");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " +surname);
+         
+        }
+    }
+}
