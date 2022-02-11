b15469a49318bca4e209d5faac6b6eadacdfe869
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-11 19:23:48 +0300

    Add files via upload

---
+++ c# örnekler/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace deneme {
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("İsminizi Girin");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " +surname);
+         
+
+
+            string dt = DateTime.Now.ToString("dd.MM.yyyy");
+            Console.WriteLine(dt);
+
+            string c = DateTime.Now.ToString("HH:mm");
+            Console.WriteLine(c);
+
+        }
+    }
+}
