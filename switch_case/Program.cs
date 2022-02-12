990074a8b5840e8e478cb8c8d08bbc7beda84128
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-12 20:08:18 +0300

    Add files via upload

---
+++ switch_case/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+ namespace switch_case  {
+     class Program{
+         static void Main(string[] args)
+         {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                Console.WriteLine("Ocak ayındasın");
+                break;
+                case 2:
+                Console.WriteLine("Şubat ayındasın");
+                break;
+                case 6:
+                Console.WriteLine("Haziran ayındasın");
+                break;
+
+                
+                default:
+                Console.WriteLine("Yanlış veri girişi");
+                break;
+            }
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                Console.WriteLine("Kış mevsimindesin");
+                break;
+                case 3:
+                case 4:
+                case 5:
+                Console.WriteLine("İlkbahar mevsimindesin");
+                break;
+                case 6:
+                case 7:
+                case 8:
+                Console.WriteLine("Yaz mevsimindesin");
+                break;
+                case 9:
+                case 10:
+                case 11:
+                Console.WriteLine("Sonbahar mevsimindesin");
+                break;
+
+                default:
+                break;
+            }
+
+         }
+     }
+ }
