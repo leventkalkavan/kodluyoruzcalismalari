c2bc130c0452d0ded7c4f88ee1dc71b57931096f
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-13 12:43:08 +0300

    Add files via upload

---
+++ donguler_while_foreach/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+ namespace donguler_while_foreach {
+     class Program{
+         static void Main(string[] args)
+         {
+             Console.Write("Sayı Giriniz:");
+             int sayi = int.Parse(Console.ReadLine());
+             int sayac = 1;
+             int toplam = 0;
+            while (sayac <= sayi)
+            {
+               toplam += sayac;
+               sayac ++;
+            }
+            Console.WriteLine(toplam/sayi); 
+            
+            char karakter = 'a';
+            while (karakter <= 'z')
+            {
+                Console.Write(karakter);
+                karakter ++;
+            }
+
+            string[] araba = {"model1","model2","model3"};
+
+            foreach (var item in araba)
+            {
+                Console.WriteLine(item);                
+            }         
+
+         }
+     }
+ }
