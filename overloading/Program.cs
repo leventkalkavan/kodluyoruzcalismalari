141a124246143e41ba3e0105d034eb9f4c1f3b9f
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-14 12:55:57 +0300

    Add files via upload

---
+++ overloading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+ namespace overloading  {
+     class Program{
+         static void Main(string[] args)
+         {
+            string sayi ="123"; 
+            
+            int outSayi;
+            bool sonuc =int.TryParse(sayi, out outSayi);
+            if (sonuc)
+            {
+                Console.WriteLine("Başarılı");
+                Console.WriteLine(outSayi);
+            }
+            else
+            {
+                Console.WriteLine("BAşarısız");
+            }
+
+            Metotlar instance = new Metotlar();
+            instance.topla(2,3,out int toplamsonucu);
+            Console.WriteLine(toplamsonucu);
+
+            int ifade = 123;
+            instance.ekranayazdır(Convert.ToString(ifade));
+            instance.ekranayazdır(ifade);
+            instance.ekranayazdır("deneme","deneme2");
+
+         }
+        
+     }
+
+     class Metotlar
+     {
+         public void topla(int a,int b,out int toplam)
+         {
+             toplam = a+b;
+         }
+         
+         public void ekranayazdır(string veri)
+         {
+             Console.WriteLine(veri);
+         }
+          public void ekranayazdır(int veri)
+         {
+             Console.WriteLine(veri);
+         }
+          public void ekranayazdır(string veri1,string veri2)
+         {
+             Console.WriteLine(veri1+veri2);
+         }
+     }
+ }
