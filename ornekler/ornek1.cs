887862377b71a8a0e10db79d34de82f74600a79c
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-16 13:55:37 +0300

    Add files via upload

---
+++ ornekler/ornek1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections;
+
+ namespace ornek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList asallar = new ArrayList();
+            ArrayList asalOlmayanlar = new ArrayList();
+            Console.WriteLine("20 adet sayı girin :");
+            for (int i = 1; i < 21; i++)
+            {
+                Console.WriteLine("{0}.sayıyı girin :",i);
+                int n = int.Parse(Console.ReadLine());
+                if (negatif(n))
+                {
+                    int sayac = 0;
+                    for (int k = 2; k < n; k++)
+                    {
+                        if (n % k == 0)
+                        {
+                            sayac++;
+                        }
+
+                    }
+
+                    if (sayac == 0)
+                    {
+                        if (sayac == 1)
+                        {
+                            asalOlmayanlar.Add(n);
+                        }
+                        else
+                            asallar.Add(n);
+                    }
+
+                    else
+                    {
+                        asalOlmayanlar.Add(n);
+                    }
+                }
+
+                else
+                {
+                    System.Console.WriteLine("Pozitif ve Numeric Bir sayı Girin");
+                }
+
+
+            }
+            asallar.Sort();
+            asallar.Reverse();
+            asalOlmayanlar.Sort();
+            asalOlmayanlar.Reverse();
+
+            System.Console.WriteLine("*****Asal Sayılar*****");
+
+            foreach (int item in asallar)
+            {
+                System.Console.WriteLine(item);
+            }
+            System.Console.WriteLine("*****Asal Olmayan Sayılar*****");
+            
+            foreach (var item in asalOlmayanlar)
+            {
+                System.Console.WriteLine(item);
+            }
+
+
+            int toplam1=0;
+            int toplam2=0;
+            foreach (int item in asallar)
+            {
+                toplam1=toplam1+item;
+                    
+            }
+            Console.WriteLine("Asalların ortalaması= " + toplam1/asallar.Count+" ve" + " dizideki eleman sayısı= "+asallar.Count );
+            foreach (int item in asalOlmayanlar)
+            {
+                toplam2=toplam2+item;
+                    
+            }
+            Console.WriteLine("Asal olmayan sayıların ortalaması= " + toplam2/asalOlmayanlar.Count+" ve" + " dizideki eleman sayısı= "+asalOlmayanlar.Count );
+        }
+
+        private static bool negatif(int s)
+        {
+            bool sonuc = true;
+            if (s < 0)
+            {
+                sonuc = false;
+            }
+
+            return sonuc;
+
+        }
+
+    }
+
+}
---
+++ ornekler/ornek3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace ornek
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cümleyi girin: ");
+            string cumle = Console.ReadLine().ToLower();     
+            char[] sesliler = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };       
+            int toplam = 0;
+            foreach(char karakter in cumle)
+            foreach(char sesli in sesliler)
+            if(karakter == sesli)
+            toplam++;
+            Console.WriteLine("Cümlede {0} adet sesli harf vardır", toplam);
+            Console.WriteLine("sesli harfler ", sesliler);
+            
+        }      
+        
+    }
+}    
