92ad0a7a3eedf23a1256040eee90b4fc20fc14e5
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-17 15:01:43 +0300

    Add files via upload

---
+++ Struct/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           D diktorgen = new D();
+           diktorgen.Kk = 3;
+           diktorgen.Uk=4;
+           Console.WriteLine("Class Alan Hesabı:{0}", diktorgen.Ah());
+           
+           dDstruct dDstruct = new dDstruct();
+           dDstruct.Kk=3;
+           dDstruct.Uk=4;
+
+           Console.WriteLine("Class Alan Hesabı:{0}", dDstruct.Ah());
+        }
+        class D
+        {
+            public int Kk;
+            public int Uk;
+            public D(){
+                Kk=3;
+                Uk=4; 
+            }
+            public long Ah(){
+                 return this.Kk *this.Uk;
+
+            }
+        }
+    }
+    struct dDstruct
+    {
+
+            public int Kk;
+            public int Uk;
+            public long Ah(){
+                 return this.Kk*this.Uk;
+
+            }
+    }
+}
