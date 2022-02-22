29e02351a55458d0e5eaaa1833d23f0d09355f40
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-22 13:53:22 +0300

    Add files via upload

---
+++ terstenyazma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace terstenyazma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String cumle;
+            Console.Write("cumle giriniz:  ");
+            cumle= Console.ReadLine();
+           for (int i = 0; i<= cumle.Length - 1; i++)
+             {
+              Console.Write(cumle[cumle.Length - i - 1]);
+             }
+             Console.ReadLine();
+        }
+    }
+}
