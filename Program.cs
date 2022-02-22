a62b6b65eb0c3c6281d23762a36ec185ea0842a2
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-22 13:12:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ucgencizim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              for (int row = 1; row <= 4; ++row)
+        {
+            for (int col = 1; col <= row; ++col)
+            {
+                Console.Write("*");
+            }
+ 
+            Console.WriteLine();
+        }
+        }
+    }
+}
