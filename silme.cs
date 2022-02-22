7e53546c103d2aea89a9996fdc4c7dcb493d2d6d
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-22 13:42:16 +0300

    Add files via upload

---
+++ silme.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace silme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mesaj = "Cumle Denemesi";
+            string yeni_mesaj = mesaj.Remove( 3);
+            Console.WriteLine(yeni_mesaj);
+        }
+    }
+}
