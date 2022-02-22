e6d5b74d21a8d6e379d24cf631772cbe994a2790
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-02-22 12:45:38 +0300

    Add files via upload

---
+++ fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c;
+ 
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+ 
+            for (int i = 1; i <= 8; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+ 
+                Console.WriteLine(c/8);
+            }
+            Console.Read();
+        }
+    }
+}
