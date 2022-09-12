3ac45e1e98fb0463999a7b02fc71fbaf505fb06b
Levent Kalkavan <87976726+leventkalkavan@users.noreply.github.com>
2022-09-12 12:00:06 +0300

    Add files via upload

---
+++ reactdeneme/callback.js
@@ -0,0 +1,10 @@
+// setTimeout(()=>{
+// console.log("merhaba");
+// },2000);
+// setInterval(()=>{
+//     console.log("her saniye göster.");
+// },1000);
+const merhabade = () => {
+    console.log("merhabaa");
+}
+merhabade();
---
+++ reactdeneme/index.js
@@ -0,0 +1,4 @@
+import GetData from "./my-module.js"
+
+const rValue= await GetData(1);
+console.log(rValue);
